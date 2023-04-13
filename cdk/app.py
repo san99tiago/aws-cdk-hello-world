d4779477009840f88c85c6e01cca0ecbd31512ae
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2023-04-12 19:49:28 -0500

    Update solution with better entrypoint app.py description and added CF outputs

--- cdk/app.py
+++ cdk/app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
-
 from stacks.cdk_hello_world_stack import CdkHelloWorldStack
 
 
 app = cdk.App()
-hello_world_stack = CdkHelloWorldStack(app, "CdkHelloWorldStack")
+hello_world_stack = CdkHelloWorldStack(
+    app,
+    "CdkHelloWorldStack",
+    env={
+        "account": os.getenv("CDK_DEFAULT_ACCOUNT"),
+        "region": os.getenv("CDK_DEFAULT_REGION"),
+    },
+    description="Stack for a simple CDK hello-world app",
+)
 
 cdk.Tags.of(hello_world_stack).add("Environment", "Development")
 cdk.Tags.of(hello_world_stack).add("RepositoryUrl", "https://github.com/san99tiago/aws-cdk-hello-world")
--- cdk/stacks/cdk_hello_world_stack.py
+++ cdk/stacks/cdk_hello_world_stack.py
@@ -4,13 +4,21 @@ from aws_cdk import (
     Stack,
     Duration,
     aws_lambda,
+    CfnOutput,
 )
 from constructs import Construct
 
 class CdkHelloWorldStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.construct_id = construct_id
 
+        # Main methods to create resources
+        self.create_lambda_function()
+        self.add_lambda_function_url()
+        self.generate_cloudformation_outputs()
+
+    def create_lambda_function(self):
         # Get relative path for folder that contains Lambda function sources
         # ! Note--> we must obtain parent dirs to create path (that's why there is "os.path.dirname()")
         top_level_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -19,11 +27,10 @@ class CdkHelloWorldStack(Stack):
             "lambda",
             "src",
         )
-        print("Source code for lambda function obtained from: ", PATH_TO_FUNCTION_FOLDER)
 
         self.simple_lambda = aws_lambda.Function(
             self,
-            id="{}Lambda".format(construct_id),
+            id="{}Lambda".format(self.construct_id),
             function_name="hello-world-lambda-function",
             description="Simple hello world function deployed with AWS CDK",
             code=aws_lambda.Code.from_asset(PATH_TO_FUNCTION_FOLDER),
@@ -34,6 +41,18 @@ class CdkHelloWorldStack(Stack):
             environment={"LOG_LEVEL": "DEBUG"},  # "INFO" or "DEBUG" (based on log_level requirements)
         )
 
-        self.simple_lambda.add_function_url(
+    def add_lambda_function_url(self):
+        self.lambda_function_url = self.simple_lambda.add_function_url(
             auth_type=aws_lambda.FunctionUrlAuthType.NONE,
+        )
+
+    def generate_cloudformation_outputs(self):
+        """
+        Method to create/add the relevant CloudFormation outputs.
+        """
+        CfnOutput(
+            self,
+            "LambdaFunctionUrl",
+            value=self.lambda_function_url.url,
+            description="URL to invoke Lambda Function",
         )
